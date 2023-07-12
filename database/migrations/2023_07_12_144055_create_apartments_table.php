e81713904b3680d6dc20ea1112ed677b20b9e036
LorenzoMegna24 <megnalorenzo0@gmail.com>
2023-07-12 17:09:17 +0200

    Aggiuntacolonne appart. e relazione con users

--- database/migrations/2023_07_12_144055_create_apartments_table.php
+++ database/migrations/2023_07_12_144055_create_apartments_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->smallInteger('room');
+            $table->smallInteger('bathroom');
+            $table->smallInteger('bed');
+            $table->integer('sq_meters')->nullable();
+            $table->string('address');
+            $table->integer('longitude');
+            $table->integer('latitude');
+            $table->string('image');
+            $table->boolean('visibility')->default(1)->change();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
