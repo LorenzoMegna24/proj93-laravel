6fd1bbef1b1966ed0450a29a400e611be59f2eb0
alecasentini <ale.casentini@gmail.com>
2023-07-13 12:24:40 +0200

    Aggiunto Amenityseeder inizio

--- database/seeders/AmenitySeeder.php
+++ database/seeders/AmenitySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin\Amenity;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,74 @@ class AmenitySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $amenities = [
+            [
+                'name' => 'WiFi',
+                'image' => ''
+            ],
+            [
+                'name' => 'TV',
+                'image' => ''
+            ],
+            [
+                'name' => 'Parcheggio',
+                'image' => ''
+            ],
+            [
+                'name' => 'Piscina',
+                'image' => ''
+            ],
+            [
+                'name' => 'Aria Condizionata',
+                'image' => ''
+            ],
+            [
+                'name' => 'Phon',
+                'image' => ''
+            ],
+            [
+                'name' => 'Palestra',
+                'image' => ''
+            ],
+            [
+                'name' => 'Biancheria',
+                'image' => ''
+            ],
+            [
+                'name' => 'Sauna',
+                'image' => ''
+            ],
+            [
+                'name' => 'Grucce',
+                'image' => ''
+            ],
+            [
+                'name' => 'Cassaforte',
+                'image' => ''
+            ],
+            [
+                'name' => 'Lavatrice',
+                'image' => ''
+            ],
+            [
+                'name' => 'Cucina',
+                'image' => ''
+            ],
+            [
+                'name' => 'Ascensore',
+                'image' => ''
+            ],
+            [
+                'name' => 'Accesso Disabili',
+                'image' => ''
+            ],
+        ];
+
+        foreach ($amenities as $elem) {
+            $new_sponsor = new Amenity();
+            $new_sponsor->name = $elem['name'];
+            $new_sponsor->image = $elem['image'];
+            $new_sponsor->save();
+        }
     }
 }
